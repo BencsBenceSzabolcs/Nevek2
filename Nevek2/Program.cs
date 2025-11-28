bb2b1565c0b18218c03a698bf76923360a1c588d
BBence <bencsb@kkszki.hu>
2025-11-28 09:12:41 +0100

    Add project files.

---
+++ Nevek2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Nevek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("0 Kilépés");
+                Console.WriteLine("1 Néveleji szóközök levágva");
+                Console.WriteLine("2 Csak nagybetűvel kezdődők");
+                Console.WriteLine("3 Csak számjegy nélküliek");
+                Console.WriteLine("4 Csak speciális jel nélküliek");
+                Console.WriteLine("5 Legalább egy szóközt tartalmazók");
+                Console.WriteLine("6 Csak rendes nevek");
+                string valaszt = Console.ReadLine();
+
+                if(valaszt == "0")
+                {
+                    return;
+                }
+
+            }
+        }
+    }
+}
